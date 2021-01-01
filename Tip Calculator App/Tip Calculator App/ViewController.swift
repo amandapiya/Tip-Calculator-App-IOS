1a79e0a151e157d8a04b0c4faa0b5913e9f54a81
amandapiya <apiyapan@caltech.edu>
2021-01-01 17:13:08 -0600

    template code

---
+++ Tip Calculator App/Tip Calculator App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator App
+//
+//  Created by Amanda P on 1/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
